64341eb9efa1f15df8bb8158257b68b0d903ccf6
ab.yf <abdullahyusuf.bmiib@gmail.com>
2024-01-31 00:48:20 +0300

    removal of 'getConnection()' call and using pool.

--- angular-ecom-server/index.js
+++ angular-ecom-server/index.js
@@ -27,22 +27,20 @@ app.get ('/', (req, res) => {
     // };
     // res.status(200).send(prodData);
 
-
     // Takes callback function, err represents error occured during establishing connection.
     // connection is a poolConnection object through which multiple database operations can be performed.
-    pool.getConnection((err, connection) => {
-        if (err) {
-        res.status(500).send(err);
-        } else {
-        pool.query('select * from categories', (error, categories) => {
-            if (error) {
-                res.status(500).send(error);
-            }else {
-                //We want to send the categories to our client.
-                res.status(200).send(categories);
-            }
-        });
-        // res.status(200).send('Connection Established');
+
+    // We want to use Pool instead of connection because it allows multiple connections to be handled at once, is quicker, no need for getConnection()
+    // and handles the connection , query and release stages by pool.query().
+
+
+    // if there is an error during connection phase, that will be handled by the "pool.query()"
+    pool.query('select * from categories', (error, categories) => {
+        if (error) {
+            res.status(500).send(error);
+        }else {
+            //We want to send the categories to our client.
+            res.status(200).send(categories);
         }
     });
 });
