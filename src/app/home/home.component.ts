8b67e4f05a45694a701d26d0c2bc6469ce358262
ab.yf <abdullahyusuf.bmiib@gmail.com>
2024-01-30 20:23:30 +0300

    Adding Product Comp and 1st Product.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CategorynavComponent } from './components/categorynav/categorynav.component';
 import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
+import { ProductsComponent } from './components/products/products.component';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-homehome',
   standalone: true,
-  imports: [HeaderComponent, CategorynavComponent, SidenavigationComponent, CommonModule, FontAwesomeModule],
+  imports: [HeaderComponent, CategorynavComponent, SidenavigationComponent, ProductsComponent, CommonModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
