175de9888114beb42f31a7eef26ea3d1e504d61c
ab.yf <abdullahyusuf.bmiib@gmail.com>
2024-01-30 19:27:01 +0300

    Added Side Navigation Bar w/style and '@mixin'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CategorynavComponent } from './components/categorynav/categorynav.component';
+import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-homehome',
   standalone: true,
-  imports: [HeaderComponent, CategorynavComponent, CommonModule, FontAwesomeModule],
+  imports: [HeaderComponent, CategorynavComponent, SidenavigationComponent, CommonModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
