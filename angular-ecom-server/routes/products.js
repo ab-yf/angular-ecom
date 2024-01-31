3f025b29cd64df0f503ec62d7d740a7331151eaf
ab.yf <abdullahyusuf.bmiib@gmail.com>
2024-01-31 04:44:13 +0300

    'products/1', products/2'...

--- angular-ecom-server/routes/products.js
+++ angular-ecom-server/routes/products.js
@@ -16,6 +16,7 @@ products.get('/', (req, res) => {
 
 
 // We now have a route which accepts a product ID as a parameter
+// Here, :id specifices route paramter and we extract that param in local 'id' variable using req.params.id.
 products.get('/(:id)', (req, res) => {
   let id = req.params.id;
   pool.query('select * from products where id = ' + id, (error, products) => {
